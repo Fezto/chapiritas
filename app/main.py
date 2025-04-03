5c50f4110ff118d003d3a30bff7b17aac44632a0
Fezto <ayrtonsepch@gmail.com>
2025-04-02 20:46:49 -0600

    CORS

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from app.api.routers.user import router as users_router
 from app.api.routers.product import router as products_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="API de ejemplo para chacharitas")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://chacharitas.org"],  # Cambia esto según tu dominio en producción
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Incluye el router de usuarios
 app.include_router(users_router, prefix="", tags=["users"])
